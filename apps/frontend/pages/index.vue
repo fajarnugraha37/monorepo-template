5d979bb618f3bda4f5fb79ea140949454d4cb836
fajar <nugrahafajar37@gmail.com>
2025-04-20 03:24:58 +0700

    feat(frontend): add sample page and api for ssr

---
+++ apps/frontend/pages/index.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+const { data } = await useFetch('/api/hello');
+</script>
+
+<template>
+  <h1>Index page {{ data.message }}</h1>
+  <NuxtLink to="/blog/hello-world"> Go to blog post </NuxtLink>
+</template>
